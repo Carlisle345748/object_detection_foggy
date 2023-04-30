883378756e9b93b6e77b988025f37a29ef454f79
Zikun Cui <czk86606733@gmai.com>
2023-04-29 17:07:20 -0700

    1. add Ensemble Model 2. Modify teacher student trainer initialization

--- trainer/teacher_student.py
+++ trainer/teacher_student.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import weakref
 from abc import ABC
 
@@ -8,8 +9,10 @@ from detectron2.evaluation import COCOEvaluator
 from detectron2.utils import comm
 from detectron2.utils.logger import setup_logger
 
+from model.EnsembleModel import EnsembleModel
 
-class BaselineTrainer(DefaultTrainer, ABC):
+
+class TeacherStudentTrainer(DefaultTrainer, ABC):
 
     def __init__(self, cfg):
         TrainerBase.__init__(self)
@@ -19,19 +22,31 @@ class BaselineTrainer(DefaultTrainer, ABC):
         cfg = DefaultTrainer.auto_scale_workers(cfg, comm.get_world_size())
 
         # Assume these objects must be constructed in this order.
-        model = self.build_model(cfg)
-        optimizer = self.build_optimizer(cfg, model)
+        student_model = self.build_model(cfg)
+        optimizer = self.build_optimizer(cfg, student_model)
         data_loader = self.build_train_loader(cfg)
 
-        model = create_ddp_model(model, broadcast_buffers=False)
+        student_model = create_ddp_model(student_model, broadcast_buffers=False)
+
+        # ***********************************************************************************
+
+        # Create teacher model
+        teacher_model = self.build_model(cfg)
+        self.teacher_model = teacher_model
+
+        # Ensemble model allow both teacher and student model to be saved and loaded together
+        ensemble_model = EnsembleModel(teacher_model, student_model)
+
+        # ***********************************************************************************
+
         self._trainer = (AMPTrainer if cfg.SOLVER.AMP.ENABLED else SimpleTrainer)(
-            model, data_loader, optimizer
+            student_model, data_loader, optimizer
         )
 
         self.scheduler = self.build_lr_scheduler(cfg, optimizer)
         self.checkpointer = DetectionCheckpointer(
             # Assume you want to save checkpoints together with logs/statistics
-            model,
+            ensemble_model,
             cfg.OUTPUT_DIR,
             trainer=weakref.proxy(self),
         )
