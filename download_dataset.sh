aaee1fcc55d9521c4f0cf385cf0132812ac44abb
Zikun Cui <czk86606733@gmai.com>
2023-04-30 00:55:16 -0700

    Download depth dataset

--- download_dataset.sh
+++ download_dataset.sh
@@ -21,16 +21,17 @@ if [ ! -d "datasets/cityscapes/leftImg8bit" ]; then
     download_and_unzip cityscapes leftImg8bit_trainvaltest.zip
 fi
 
+if [ ! -d "datasets/disparity" ]; then
+    download_and_unzip disparity disparity_trainvaltest.zip
+fi
+
 if [ ! -d "datasets/cityscapes_foggy/leftImg8bit" ]; then
     download_and_unzip cityscapes_foggy leftImg8bit_trainval_foggyDBF.zip
     mv datasets/cityscapes_foggy/leftImg8bit_foggyDBF datasets/cityscapes_foggy/leftImg8bit
     rm datasets/cityscapes_foggy/foggy_trainval_refined_filenames.txt
+    rm datasets/cityscapes_foggy/README_foggyDBF.md
 fi
 
 if [ ! -d "datasets/cityscapes_foggy/gtFine" ]; then
     cp -r datasets/cityscapes/gtFine datasets/cityscapes_foggy/gtFine
 fi
-
-
-
-
