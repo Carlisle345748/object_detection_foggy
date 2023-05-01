161a1e6fe9d9ec18578bda4ec6e0ae50980aaad8
Zikun Cui <czk86606733@gmai.com>
2023-05-01 01:47:34 -0700

    Add depth trainer

---
+++ trainer/depth_trainer.py
@@ -0,0 +1,20 @@
+import os
+from abc import ABC
+
+from detectron2.data import build_detection_train_loader
+from detectron2.engine import DefaultTrainer
+from detectron2.evaluation import COCOEvaluator
+
+from data.dataset_mapper import DepthDatasetMapper
+
+
+class DepthTrainer(DefaultTrainer, ABC):
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name):
+        return [COCOEvaluator(dataset_name, tasks=("bbox",), output_dir=os.path.join(cfg.OUTPUT_DIR, "inference"))]
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        mapper = DepthDatasetMapper(cfg)
+        return build_detection_train_loader(cfg, mapper=mapper)
