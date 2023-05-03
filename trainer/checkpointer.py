3eb7c8109fb6de633456a415e3f385387e18ea1e
Zikun Cui <czk86606733@gmai.com>
2023-05-02 23:57:15 -0700

    Support teacher student model initialization

---
+++ trainer/checkpointer.py
@@ -0,0 +1,42 @@
+from typing import Dict, Any
+
+from detectron2.checkpoint import DetectionCheckpointer
+
+from model.teacher_student import TeacherStudentRCNN
+
+
+class TeacherStudentCheckpointer(DetectionCheckpointer):
+    def __init__(self, cfg, model, save_dir="", *, save_to_disk=None, **checkpointables):
+        super().__init__(model=model, save_dir=save_dir, save_to_disk=save_to_disk, **checkpointables)
+
+        self.base_model_weight = cfg.MODEL.TEACHER_STUDENT.WEIGHT
+
+    def resume_or_load(self, path: str, *, resume: bool = True) -> Dict[str, Any]:
+        if resume and self.has_checkpoint():
+            path = self.get_checkpoint_file()
+            return self.load(path)
+        else:
+            return self.init_model(path)
+
+    def init_model(self, meta_model_weight):
+        if meta_model_weight:
+            return self.load(meta_model_weight, checkpointables=[])
+
+        assert isinstance(self.model, TeacherStudentRCNN), "Model must be TeacherStudentRCNN"
+        assert self.base_model_weight, "Base model weight is not provided"
+
+        # Temporary replace the model
+        meta_model = self.model
+
+        # Load student model
+        self.model = meta_model.student
+        self.load(self.base_model_weight, checkpointables=[])
+
+        # Load teacher model
+        self.model = meta_model.teacher
+        ret = self.load(self.base_model_weight, checkpointables=[])
+
+        # Resume the model
+        self.model = meta_model
+
+        return ret
--- trainer/teacher_student.py
+++ trainer/teacher_student.py
@@ -1,14 +1,48 @@
+import logging
 import os
+import weakref
 from abc import ABC
-import model.teacher_student  # Import for registering models
 
-from detectron2.engine import DefaultTrainer
+from detectron2.engine import DefaultTrainer, create_ddp_model, AMPTrainer, SimpleTrainer
 from detectron2.evaluation import COCOEvaluator
+from detectron2.utils import comm
+from detectron2.utils.logger import setup_logger
 
 from data.domain_adaptation_dataloader import build_domain_adaptation_train_loader
+from trainer.checkpointer import TeacherStudentCheckpointer
 
 
 class TeacherStudentTrainer(DefaultTrainer, ABC):
+    def __init__(self, cfg):
+        super(DefaultTrainer, self).__init__()
+        logger = logging.getLogger("detectron2")
+        if not logger.isEnabledFor(logging.INFO):  # setup_logger is not called for d2
+            setup_logger()
+        cfg = DefaultTrainer.auto_scale_workers(cfg, comm.get_world_size())
+
+        # Assume these objects must be constructed in this order.
+        model = self.build_model(cfg)
+        optimizer = self.build_optimizer(cfg, model)
+        data_loader = self.build_train_loader(cfg)
+
+        model = create_ddp_model(model, broadcast_buffers=False)
+        self._trainer = (AMPTrainer if cfg.SOLVER.AMP.ENABLED else SimpleTrainer)(
+            model, data_loader, optimizer
+        )
+
+        self.scheduler = self.build_lr_scheduler(cfg, optimizer)
+        self.checkpointer = TeacherStudentCheckpointer(
+            # Assume you want to save checkpoints together with logs/statistics
+            cfg=cfg,
+            model=model,
+            save_dir=cfg.OUTPUT_DIR,
+            trainer=weakref.proxy(self),
+        )
+        self.start_iter = 0
+        self.max_iter = cfg.SOLVER.MAX_ITER
+        self.cfg = cfg
+
+        self.register_hooks(self.build_hooks())
 
     @classmethod
     def build_evaluator(cls, cfg, dataset_name):
@@ -17,4 +51,3 @@ class TeacherStudentTrainer(DefaultTrainer, ABC):
     @classmethod
     def build_train_loader(cls, cfg):
         return build_domain_adaptation_train_loader(cfg)
-
