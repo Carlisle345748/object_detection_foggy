7360eb7499274fcf5b66e1053d79df0f9797a313
Zikun Cui <czk86606733@gmai.com>
2023-05-05 09:21:46 -0700

    Remove torch complie

--- trainer/teacher_student.py
+++ trainer/teacher_student.py
@@ -24,11 +24,6 @@ class TeacherStudentTrainer(DefaultTrainer, ABC):
 
         # Assume these objects must be constructed in this order.
         model = self.build_model(cfg)
-
-        if torch.version.__version__ == "2.0.0":
-            logger.info("try to compile model")
-            model = torch.compile(model)
-
         optimizer = self.build_optimizer(cfg, model)
         data_loader = self.build_train_loader(cfg)
 
