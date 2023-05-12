b68833032fbd5282261c1a1bfb493c8991b34ccc
Zikun Cui <czk86606733@gmai.com>
2023-05-11 18:37:33 -0700

    fix profiler import error

--- trainer/profiler.py
+++ trainer/profiler.py
@@ -4,11 +4,12 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 from trainer.config import add_teacher_student_config
 from trainer.teacher_student import TeacherStudentTrainer
+from detectron2.utils.events import EventStorage
 
 # Define your model
 cfg = get_cfg()
 add_teacher_student_config(cfg)
-cfg.merge_from_file("config/RCNN-C4-50-TS.yaml")
+cfg.merge_from_file("config/RCNN-C4-50-TS-Test.yaml")
 
 model = TeacherStudentTrainer.build_model(cfg)
 
@@ -22,10 +23,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 # Run the profiler
-with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True) as prof:
     with record_function("model_training"):
         # Run your model
-        output = model(input)
+        with EventStorage(0):
+            output = model(input)
 
 # Print the profiler results
-print(prof.key_averages().table(sort_by="cuda_time_total"))
+print(prof.key_averages().table(sort_by="self_cuda_memory_usage"))
