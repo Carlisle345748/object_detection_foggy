84f82dfd1b512505fd2f5c2f5a8cfc16c8ec8732
Zikun Cui <czk86606733@gmai.com>
2023-05-12 00:54:59 -0700

    add SILog Error delta_threshold to depth evaluator

--- evaluator/depth_evaluator.py
+++ evaluator/depth_evaluator.py
@@ -4,10 +4,17 @@ import torch
 
 
 class DepthEvaluator(DatasetEvaluator):
+    """
+    DepthEvaluator inherits from DatasetEvaluator. It is used to evaluate the performance of a depth estimation model. The class has the following methods:
+    """
     def __init__(self):
         self.mse = 0
         self.mae = 0
         self.count = 0
+        self.silog = 0
+        self.delta1 = 0
+        self.delta2 = 0
+        self.delta3 = 0
 
     def reset(self):
         self.mse = 0
@@ -18,29 +25,45 @@ class DepthEvaluator(DatasetEvaluator):
         for data, pred in zip(inputs, outputs):
             gt_depth_map = data["depth"]
             pred_depth_map = pred["depth"]
-            self.mse = F.mse_loss(pred_depth_map, gt_depth_map).to("cup")
-            self.mae = torch.mean(torch.abs(pred_depth_map - gt_depth_map)).to("cup")
-
+            self.mse += F.mse_loss(pred_depth_map, gt_depth_map).to("cup").item()
+            self.mae += torch.mean(torch.abs(pred_depth_map - gt_depth_map)).to("cup").item()
+            self.silog += self.silog_loss(pred_depth_map, gt_depth_map).to("cup").item()
+            self.delta1 += self.delta_threshold(gt_depth_map, pred_depth_map, 1.25).to("cup").item()
+            self.delta2 += self.delta_threshold(gt_depth_map, pred_depth_map, 1.25 ** 2).to("cup").item()
+            self.delta3 += self.delta_threshold(gt_depth_map, pred_depth_map, 1.25 ** 3).to("cup").item()
+            
             self.count += 1
 
     def evaluate(self):
         return {
             "Depth MSE": self.mse / self.count,
+            "Depth MAE": self.mae / self.count,
+            "Depth SILog Error": self.silog / self.count,
+            "Depth Delta1": self.delta1 / self.count,
+            "Depth Delta2": self.delta2 / self.count,
+            "Depth Delta3": self.delta3 / self.count,
         }
-    
-    def silog_loss(self, pred_depth: torch.Tensor, gt_depth: torch.Tensor):
+
+    @classmethod
+    def silog_loss(cls, pred_depth: torch.Tensor, gt_depth: torch.Tensor):
         """
         Computes the Scale-Invariant Logarithmic Error (SILog) between the predicted and ground truth depth maps.
         """
-        gt_depth_log = torch.log(torch.clamp(gt_depth, min=1e-6))  
-        pred_depth_log = torch.log(torch.clamp(pred_depth, min=1e-6)) 
-        
+        gt_depth_log = torch.log(torch.clamp(gt_depth, min=1e-6))
+        pred_depth_log = torch.log(torch.clamp(pred_depth, min=1e-6))
+
         log_diff = gt_depth_log - pred_depth_log
         N = gt_depth.numel()  # Number of pixels
-        
+
         squared_error = torch.sum(log_diff ** 2) / N
         squared_mean_error = torch.sum(log_diff) ** 2 / (N ** 2)
-        
+
         silog_loss = squared_error - squared_mean_error
 
         return silog_loss
+
+    @classmethod
+    def delta_threshold(cls, y_true: torch.Tensor, y_pred: torch.Tensor, delta: float):
+        ratio = torch.maximum(y_true / y_pred, y_pred / y_true)
+        within_threshold = (ratio < delta).float()
+        return torch.mean(within_threshold)
