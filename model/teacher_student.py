815eeee0f2edcac609c3b6cd161bc9fe6fedd342
Anny <superanny108@gmail.com>
2023-05-21 17:34:24 +0000

    move pseudo label to cpu

--- model/teacher_student.py
+++ model/teacher_student.py
@@ -201,9 +201,10 @@ class TeacherStudentRCNN(nn.Module):
     @classmethod
     def add_pseudo_labels(cls, batch_inputs, pseudo_labels):
         for data, instance in zip(batch_inputs, pseudo_labels):
+            instance = instance.to("cpu")
             gt_instances = Instances(instance.image_size)
-            gt_instances.gt_boxes = instance.pred_boxes.cpu()
-            gt_instances.gt_classes = instance.pred_classes.cpu()
+            gt_instances.gt_boxes = instance.pred_boxes
+            gt_instances.gt_classes = instance.pred_classes
             data["instances"] = gt_instances
         return batch_inputs
 
